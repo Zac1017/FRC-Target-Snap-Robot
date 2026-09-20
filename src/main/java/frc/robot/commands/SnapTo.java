07fd723b03a31cc8a3168edc5ffd9ce3724b1a00
Zac1017 <9964262@bedfordnhk12.net>
2026-09-20 14:59:10 -0400

    Remove unused import of Pose2d in SnapTo command

--- src/main/java/frc/robot/commands/SnapTo.java
+++ src/main/java/frc/robot/commands/SnapTo.java
@@ -4,7 +4,6 @@ import java.util.function.Supplier;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
